dd3262bedff831efba227bfdd96732902130bacd
Chief <luyolo.nqobile@gmail.com>
2020-01-17 17:49:59 +0200

    Pretrained Word Vectors
    Download FB15k entity names as tsv.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+wget https://drive.google.com/file/d/0B52yRXcdpG6MaHA5ZW9CZ21MbVk/view -O ./data/FB15k/mid2name.tsv
 wget http://nlp.stanford.edu/data/glove.twitter.27B.zip -O ./language_models/glove/glove.twitter.27B.zip
