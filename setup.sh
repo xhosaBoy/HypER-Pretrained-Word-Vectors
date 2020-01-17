2c902c36cb86398cfc326a25cf001e071df19ca9
Chief <luyolo.nqobile@gmail.com>
2020-01-17 19:29:59 +0200

    Pretrained Word Vectors
    Compressed FB15k entity id to name map and updated setup.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget https://drive.google.com/file/d/0B52yRXcdpG6MaHA5ZW9CZ21MbVk/view -O ./data/FB15k/mid2name.tsv
+wget https://drive.google.com/drive/u/0/folders/13-_KyI8vhW2LztXMdR-JuFqKFSl9UTaw -O /tmp/mid2name.tsv
 wget http://nlp.stanford.edu/data/glove.twitter.27B.zip -O ./language_models/glove/glove.twitter.27B.zip
+sudo apt install unzip
+unzip language_models/glove/glove.twitter.27B.zip
