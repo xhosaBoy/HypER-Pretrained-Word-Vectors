0e30c600ee22e1d7d830a3bc3ddc674d2fa8b2b2
Chief <luyolo.nqobile@gmail.com>
2020-02-01 16:25:48 +0200

    HypER+ With Pre-trained Word Vectors FB15k-237 Results
    Update setup.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+pip install -r requirements.txt
+sudo apt install unzip
+
 gunzip data/WN18/synsetid2name.tsv.gz
 gunzip data/FB15k/mid2name.tsv.gz
+
 wget http://nlp.stanford.edu/data/glove.6B.zip -O HypER/language_models/glove/glove.6B.zip
-wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz -O HypER/language_models/fasttext/cc.en.300.bin.gz
-sudo apt install unzip
-unzip HypER/language_models/glove/glove.twitter.27B.zip -d HypER/language_models/glove/
-gunzip -c HypER/language_models/fasttext/cc.en.300.bin.gz > HypER/language_models/fasttext/cc.en.300.bin
-pip install -r requirements.txt
+unzip HypER/language_models/glove/glove.6B.zip -d HypER/language_models/glove/
+# wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz -O HypER/language_models/fasttext/cc.en.300.bin.gz
+# gunzip -c HypER/language_models/fasttext/cc.en.300.bin.gz > HypER/language_models/fasttext/cc.en.300.bin
+
 python HypER/language_models/attribute_mapper.py
 python HypER/language_models/language_model_manager.py
