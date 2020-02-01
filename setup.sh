531a6212ac93f04aabeddca674a630708d8b7c1d
Chief <luyolo.nqobile@gmail.com>
2020-02-01 17:15:18 +0200

    HypER+ With Pre-trained Word Vectors FB15k-237 Results
    Download Fasttext language model during setup.

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ gunzip data/FB15k/mid2name.tsv.gz
 
 wget http://nlp.stanford.edu/data/glove.6B.zip -O HypER/language_models/glove/glove.6B.zip
 unzip HypER/language_models/glove/glove.6B.zip -d HypER/language_models/glove/
-# wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz -O HypER/language_models/fasttext/cc.en.300.bin.gz
-# gunzip -c HypER/language_models/fasttext/cc.en.300.bin.gz > HypER/language_models/fasttext/cc.en.300.bin
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz -O HypER/language_models/fasttext/cc.en.300.bin.gz
+gunzip -c HypER/language_models/fasttext/cc.en.300.bin.gz > HypER/language_models/fasttext/cc.en.300.bin
 
 python HypER/language_models/attribute_mapper.py
 python HypER/language_models/language_model_manager.py
